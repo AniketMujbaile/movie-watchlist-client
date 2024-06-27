04679003a469144b90db3d60466f0cb52665c2ec
Aniket Mujbaile <81378709+AniketMujbaile@users.noreply.github.com>
2024-06-27 15:55:10 +0530

    Update HomePage.js

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,16 +1,19 @@
-import React, { useEffect } from 'react';
+ import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { fetchMovies } from '../redux/actions';  
+import { fetchMovies } from '../redux/actions';
 import MovieItem from '../components/MovieItem';
 import { Link } from 'react-router-dom';
 
 const HomePage = () => {
   const dispatch = useDispatch();
   const movies = useSelector(state => state.movies);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    dispatch(fetchMovies());  
-  }, [dispatch]); 
+    dispatch(fetchMovies())
+      .then(() => setLoading(false))
+      .catch(() => setLoading(false));
+  }, [dispatch]);
 
   return (
     <div className="bg-gray-600 min-h-screen">
@@ -20,18 +23,24 @@ const HomePage = () => {
           <span className="text-yellow-400 font-bold text-2xl">WatchList</span>
         </Link>
         <Link to="/add" className="bg-yellow-400 hover:bg-yellow-500 font-semibold px-6 py-2 rounded-full">
-            Add Movie
+          Add Movie
         </Link>
       </nav>
       <div className="p-6">
         <div className="flex justify-center items-center mb-4">
           <h1 className="text-2xl font-bold text-black">My Watchlist</h1>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {movies.map(movie => (
-            <MovieItem key={movie.id} movie={movie} />
-          ))}
-        </div>
+        {loading ? (
+          <div className="flex justify-center items-center h-64">
+            <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-yellow-400"></div>
+          </div>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            {movies.map(movie => (
+              <MovieItem key={movie.id} movie={movie} />
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
